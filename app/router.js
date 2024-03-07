a7f1e116bb56a2546f61d9c37606e61a61c9f765
elenareboud <elena.reboud@gmail.com>
2024-03-07 17:12:49 +0100

    data.js, views

--- app/router.js
+++ app/router.js
@@ -1,8 +1,5 @@
-import express from 'express';
-import mainController from './controllers/mainController.js';
-import websiteController from './controllers/websiteController.js';
-import authController from './controllers/authController.js';
 import userController from './controllers/userController.js';
+import isLogged from './middlewares/isLogged.js';
 
 const router = express.Router();
 
@@ -21,12 +18,20 @@ router.get('/connexion', authController.login);
 router.post('/connexion', authController.loginAction);
 router.get('/inscription', authController.signup);
 router.post('/inscription', authController.signupAction);
-router.get('/deconnexion', authController.logout);
+router.get('/deconnexion', isLogged, authController.logout);
 
-router.get('/profil', userController.profil);
+router.get('/profil', isLogged, userController.profil);
+
+// on appelle endpoint une route, une url qui donnera lieu à un résultat dans notre api
+// notre api est une liste de endpoint (d'adresses) qui donneront lieu à un résultat
+router.get('/api/website', websiteController.allJson);
+router.get('/api/website/:id', websiteController.detailsJson);
+router.post('/api/website', websiteController.create);
+router.patch('/api/website/:id', websiteController.update);
+router.delete('/api/website/:id', websiteController.delete);
+
+// router.get('/api/user')
 
 router.use(mainController.notFound);
 
-
-//METTRE TOUTJOURS EN DERNIER (erreur)
 export default router;
